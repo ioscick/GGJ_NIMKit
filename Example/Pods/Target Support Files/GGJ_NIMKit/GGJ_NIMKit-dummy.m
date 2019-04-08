ed178037f7a1d410c485e1b58b20234ee9e3348a
shenliping <shenlp@gegejia.com>
2019-04-08 16:09:41 +0800

    文件

---
+++ Example/Pods/Target Support Files/GGJ_NIMKit/GGJ_NIMKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GGJ_NIMKit : NSObject
+@end
+@implementation PodsDummy_GGJ_NIMKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-GGJ_NIMKit_Example/Pods-GGJ_NIMKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GGJ_NIMKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GGJ_NIMKit_Example
+@end
